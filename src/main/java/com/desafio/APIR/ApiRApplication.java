f3949d52fc4bc289bea7c26082928ae365b3d57d
andreia23 <andreialima.berto08@gmail.com>
2020-05-18 23:47:00 -0300

    Projeto criado

---
+++ src/main/java/com/desafio/APIR/ApiRApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.APIR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/APIR/ApiRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.APIR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
