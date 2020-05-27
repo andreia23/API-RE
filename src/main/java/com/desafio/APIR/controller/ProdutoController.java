343c800ec4e6ed380fcd5c09e14ecf52a0647d9b
andreia23 <andreialima.berto08@gmail.com>
2020-05-27 20:20:53 -0300

    Rota de validação

--- src/main/java/com/desafio/APIR/controller/ProdutoController.java
+++ src/main/java/com/desafio/APIR/controller/ProdutoController.java
@@ -1,11 +1,15 @@
 package com.desafio.APIR.controller;
 
 
+import java.io.File;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,15 +40,28 @@ public class ProdutoController {
 	
 	 
 	@GetMapping("/produtos")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Produto> getAllProduto(){
-		return ProdutoService.TodosProduto();
+	public String getAllProduto(){
+		List<Produto> produtos = ProdutoService.TodosProduto();
+		JSONArray allDataArray = new JSONArray();
+		for(int index = 0; index < produtos.size(); index++) {
+            JSONObject eachData = new JSONObject();
+            eachData.put("IdPro", produtos.get(index).getIdpro());
+            eachData.put("Categoria", produtos.get(index).getId_categoria().getCategoria());
+            eachData.put("Descricão", produtos.get(index).getDescricao());
+            allDataArray.put(eachData);
+		}
+
+		return allDataArray.toString();
 	}
 	
-	@Produces(MediaType.APPLICATION_JSON)
 	@GetMapping("/produtos/{idpro}")
-	public ResponseEntity<Produto> getProdutoById(@PathVariable Integer idpro){
-		return ResponseEntity.ok().body(ProdutoService.ProdutoPorID(idpro));
+	public String getProdutoById(@PathVariable Integer idpro){
+		Produto retorno = ProdutoService.ProdutoPorID(idpro);
+	    JSONObject my_json = new JSONObject();
+	    my_json.put("IdPro", retorno.getIdpro());
+	    my_json.put("Descrição",retorno.getDescricao());
+	    my_json.put("Categoria",retorno.getId_categoria().getCategoria());
+		return my_json.toString();
 	}
 	
 	@PostMapping("/produtos")
@@ -67,6 +86,22 @@ public class ProdutoController {
 	public HttpStatus deleteProduto(@PathVariable Integer id){
 		this.ProdutoService.deleteProduto(id);
 		return HttpStatus.OK;
+	}
+	
+	@GetMapping("/arquivo")
+	public String arquivo(){
+
+		File file = new File("../Teste.pdf");
+		LocalDateTime date_time = LocalDateTime.now();
+	    String file_name = file.getName();
+	    
+	    JSONObject my_obj = new JSONObject();
+	    my_obj.put("file_name", file_name );
+	    my_obj.put("date_time", date_time);
+	    
+		return my_obj.toString();
+		
+		
 	}
 	
 }
